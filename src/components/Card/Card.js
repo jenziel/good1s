923ec77fb5d1b387e1a9b03bc9bfd0bbd105906f
Jen Ziel <jenziel400@gmail.com>
2023-11-11 17:15:05 -0500

    Add descriptive theater name.  Add conditional rendering in case a showtime is missing a year, director, or contains multiple titles within the same showtime

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -21,7 +21,7 @@ function Card({
       !isFavorited ? setIsFavorited(true) : setIsFavorited(false);
     }
   };
-
+  const dayOfWeekNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
   return (
     <div className='card'>
       <div className='favorites-btn-container'>
@@ -41,32 +41,41 @@ function Card({
           )}
         </button>
       </div>
-      <div className='card-info' id={key}>
-        <h2>{showtime.theater}</h2>
-        <p>{dayjs(showtime.date).format("MMM. D YYYY | hh:mm A")}</p>
-
+     <div className='card-info' id={key}>
+        <p className='card-theaterName'>{showtime.theaterName}</p>
         {showtime.movies.length === 1 ? (
-          showtime.movies[0].director ? (
-            <p>
+          showtime.movies[0].director.trim() ? (
+            <div>
+            <p className='card-title'>
             {" "}
-            {showtime.movies[0].title}, {showtime.movies[0].year} | dir. by {" "}
-            {showtime.movies[0].director} 
+            {showtime.movies[0].title}
+            {showtime.movies[0].year ? `, ${showtime.movies[0].year}` : ""}
           </p>
+          <p className='card-director'>dir. by {" "}
+            {showtime.movies[0].director} </p>
+            </div>
           )
-         : (<p>
+          : (<p className='card-title'>
           {" "}
-          {showtime.movies[0].title}, {showtime.movies[0].year}
-        </p>) ) : (
-
+          {showtime.movies[0].title} 
+          {showtime.movies[0].year ? `, ${showtime.movies[0].year}` : ""}
+        </p>) ) :(
+          
           <div>
             {showtime.movies.map((movie) => (
-              <p key={movie.title}>
+              <p key={movie.title} className='card-title'>
                 {" "}
-                {movie.title}, {movie.year} | dir. by {movie.director})
+                {movie.title}, {movie.year} | dir. by {movie.director}
               </p>
             ))}
           </div>
         )}
+        
+        <div className='timeBox'>
+
+        <p className='card-date'>{dayOfWeekNames[dayjs(showtime.date).day()]} {dayjs(showtime.date).format("MMM. D")}</p>
+        <p className='card-time'>{dayjs(showtime.date).format("hh:mm A")}</p>
+        </div>
       </div>
     </div>
   );
