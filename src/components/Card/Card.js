9d59c8d0a4b0978c78d96122af87b4251aeee3e5
Jen Ziel <jenziel400@gmail.com>
2023-11-11 15:25:18 -0500

    Add code so far for rendering today's showtimes

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,46 +1,72 @@
 import React from "react";
 import heartFilled from "../../images/heartFilled.png";
-import heartOutline from "../../images/heartOutline.png"
+import heartOutline from "../../images/heartOutline.png";
 import "./Card.css";
 import "dayjs/locale/en";
-import {useState} from 'react'
+import { useState } from "react";
 
-function Card({ title, year, director, date, key }) {
+function Card({
+  //  title, year, director, date,
+  key,
+  showtime,
+  favorites,
+  setFavorites,
+}) {
   const dayjs = require("dayjs");
   dayjs.locale("en");
-  const [isFavorited, setIsFavorited] = useState(false)
+  const [isFavorited, setIsFavorited] = useState(false);
 
   const handleClick = () => {
-    setIsFavorited(true)
-  }
-  
+    {
+      !isFavorited ? setIsFavorited(true) : setIsFavorited(false);
+    }
+  };
+
   return (
     <div className='card'>
-    <div className='favorites-btn-container'>
-      <button className='favorites-btn' onClick={handleClick}>
-        {!isFavorited ? (
-
+      <div className='favorites-btn-container'>
+        <button className='favorites-btn' onClick={handleClick}>
+          {!isFavorited ? (
             <img
               src={heartOutline}
               alt='heart outline'
               className='favorites-img'
             ></img>
-        ) : (
+          ) : (
             <img
-            src={heartFilled}
-            alt='heart outline'
-            className='favorites-img'
-          ></img>
-        )
-
-        }
-      </button>
-    </div>
+              src={heartFilled}
+              alt='heart outline'
+              className='favorites-img'
+            ></img>
+          )}
+        </button>
+      </div>
       <div className='card-info' id={key}>
-        <p>{dayjs(date).format("MMM. D YYYY | hh:mm A")}</p>
-        <p>
-          {title}, {year} | dir. by {director}
-        </p>
+        <h2>{showtime.theater}</h2>
+        <p>{dayjs(showtime.date).format("MMM. D YYYY | hh:mm A")}</p>
+
+        {showtime.movies.length === 1 ? (
+          showtime.movies[0].director ? (
+            <p>
+            {" "}
+            {showtime.movies[0].title}, {showtime.movies[0].year} | dir. by {" "}
+            {showtime.movies[0].director} 
+          </p>
+          )
+         : (<p>
+          {" "}
+          {showtime.movies[0].title}, {showtime.movies[0].year}
+        </p>) ) : (
+
+          <div>
+            {showtime.movies.map((movie) => (
+              <p key={movie.title}>
+                {" "}
+                {movie.title}, {movie.year} | dir. by {movie.director})
+              </p>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -3,34 +3,47 @@ import "./CardContainer.css";
 import Card from "../Card/Card";
 import { Link } from "react-router-dom";
 import NavBar from "../NavBar/NavBar";
-function CardContainer({ vidiotsShowtimes, setSelectedDate, selectedDate }) {
+function CardContainer({ theaterData, setSelectedDate, selectedDate, favorites, setFavorites }) {
   const dayjs = require('dayjs')
   dayjs.locale('en');
-  const showtimeCards = vidiotsShowtimes.map((showtime, index) => {
-    return (
-      <Card
-        title={showtime.movies[0].title}
-        year={showtime.movies[0].year}
-        director={showtime.movies[0].director}
-        date={showtime.date}
-        key={showtime.key}
-        id={showtime.id}
-      />
-    );
-  });
+
+  const justSelectedDay = theaterData.reduce((acc, theater) => {
+    theater.showtimes.forEach(showtime => {
+    const showtimeDate = dayjs(showtime.date).format("YYYY-MM-DD");
+    const selectedDateFormatted = dayjs(selectedDate).format("YYYY-MM-DD");
+      if (showtimeDate === selectedDateFormatted) {
+       return acc.push(showtime)
+      }
+    })
+    return acc
+  }, [])
+  console.log('justSelectedDay', justSelectedDay)
+  
+ const showtimeCards = 
+   justSelectedDay.map(showtime => {
+      return (
+        <Card
+        showtime={showtime}
+          key={showtime.key}
+          // id={showtime.id}
+          favorites={favorites}
+          setFavorites={setFavorites}
+        />
+      );
+
+} )
+    
+  
   return (
     <div>
-      {/* <div className='showtimes-upper'>
-        <p>day before</p>
-        <h1 className='page-title'>today</h1>
-        <p>next day</p>
-      </div> */}
       <NavBar setSelectedDate={setSelectedDate} selectedDate={selectedDate}/>
       <Link to='/favorites'>
         <button className='nav-btn'>favorited</button>
       </Link>
       <h1>Showing screenings on {dayjs(selectedDate).format("MMM. D")}</h1>
-      <div className='cards-container'>{showtimeCards}</div>
+      <div className='cards-container'>
+        {showtimeCards}
+        </div>
     </div>
   );
 }
