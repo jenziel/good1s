9ed5cc5fba6b9e25a74954a369d9198eb163cb30
Jen Ziel <jenziel400@gmail.com>
2023-11-12 15:18:28 -0500

    Fix the form validation for only dates between today and 30 days from now.

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,26 +1,38 @@
 import React from "react";
 import "./NavBar.css";
-import { NavLink } from "react-router-dom";
 import PropTypes from 'prop-types'
 
 function NavBar({setSelectedDate, selectedDate}) {
   const dayjs = require("dayjs");
   const today = dayjs();
   const date30DaysLater = today.add(30, "day");
+
+  const handleDateChange = (event) => {
+    const selectedDate = dayjs(event.target.value);
+
+    if (selectedDate.isBefore(today, 'day')) {
+      alert("Please select a date in the present or future ☺");
+    } 
+    if (selectedDate.isAfter(date30DaysLater, 'day')) {
+      alert(`Please select a date on or before ${date30DaysLater.format('MMM. D')}`)
+    } 
+    else {
+      setSelectedDate(event.target.value);
+    }
+  };
+  
   return (
     <nav>
       <form>
-
       <label htmlFor='date'>Select a date: </label>
-
       <input
         type='date'
         id='date'
         name='date'
         value={today}
-        min={today}
+        min={today.format('YYYY-MM-DD')}
         max={date30DaysLater}
-        onChange={event => setSelectedDate(event.target.value)}
+        onChange={handleDateChange}
       />
       </form>
     </nav>
