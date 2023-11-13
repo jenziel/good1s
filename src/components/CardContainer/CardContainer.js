c3dce3ab88cec0df41924f5dbb62abdf6483581b
Jen Ziel <jenziel400@gmail.com>
2023-11-13 18:05:30 -0500

    Format files with prettier.  Delete console.logs

--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -34,7 +34,6 @@ function CardContainer({
         }
       });
     }
-    console.log('acc', acc)
     return acc;
   }, []);
 
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,49 +1,48 @@
 import React from "react";
 import "./NavBar.css";
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 import utc from "dayjs/plugin/utc";
-import dayjs from "dayjs"
+import dayjs from "dayjs";
 import timezone from "dayjs/plugin/timezone";
-import {useState} from 'react'
+import { useState } from "react";
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
 
-function NavBar({setSelectedDate}) {
-dayjs.tz.setDefault('America/Los_Angeles');
-const today = dayjs().tz()
-const todayFormatted = today.format('YYYY-MM-DD')
- const date30DaysLater = today.add(30, "day").format('YYYY-MM-DD');
-  const [inputValue, setInputValue] = useState(today.format('YYYY-MM-DD'));
-console.log('today', today)
+function NavBar({ setSelectedDate }) {
+  dayjs.tz.setDefault("America/Los_Angeles");
+  const today = dayjs().tz();
+  const todayFormatted = today.format("YYYY-MM-DD");
+  const date30DaysLater = today.add(30, "day").format("YYYY-MM-DD");
+  const [inputValue, setInputValue] = useState(today.format("YYYY-MM-DD"));
 
   const handleDateChange = (event) => {
     const newSelectedDate = dayjs(event.target.value);
-    if (newSelectedDate.isBefore(today, 'day')) {
+    if (newSelectedDate.isBefore(today, "day")) {
       alert("Please select a date in the future ☺");
-    } else if (newSelectedDate.isAfter(date30DaysLater, 'day')) {
-      alert(`Please select a date on or before ${date30DaysLater.format('MMM. D')}`)
+    } else if (newSelectedDate.isAfter(date30DaysLater, "day")) {
+      alert(
+        `Please select a date on or before ${date30DaysLater.format("MMM. D")}`
+      );
     } else {
-
-      setInputValue(newSelectedDate.format('YYYY-MM-DD'));
+      setInputValue(newSelectedDate.format("YYYY-MM-DD"));
       setSelectedDate(newSelectedDate);
     }
   };
- 
-  
+
   return (
     <nav>
       <form>
-      <label htmlFor='date'>Select a date: </label>
-      <input
-        type='date'
-        id='date'
-        name='date'
-        value={inputValue}
-        min={todayFormatted}
-        max={date30DaysLater}
-        onChange={handleDateChange}
-      />
+        <label htmlFor='date'>Select a date: </label>
+        <input
+          type='date'
+          id='date'
+          name='date'
+          value={inputValue}
+          min={todayFormatted}
+          max={date30DaysLater}
+          onChange={handleDateChange}
+        />
       </form>
     </nav>
   );
