ea7ae2ee389b4522ad4acd4cdc1ed2a4baefeee6
Jen Ziel <jenziel400@gmail.com>
2023-11-12 19:54:19 -0500

    Add responsive layout

--- src/components/FavoritesPage/FavoritesPage.js
+++ src/components/FavoritesPage/FavoritesPage.js
@@ -4,25 +4,48 @@ import { Link } from "react-router-dom";
 import emptyBurst from "../../images/emptyBurst.png";
 import PropTypes from "prop-types";
 import Card from "../Card/Card"
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 function FavoritesPage({ favorites, setFavorites }) {
+  const pacificTimeZone = "America/Los_Angeles";
+  const dayOfWeekNames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
   const favoriteCards = favorites.map((showtime) => {
+    
     return (
-      <Card
-        showtime={showtime}
-        favorites={favorites}
-        setFavorites={setFavorites}
-        key={showtime['_id']}
-      />
+      <div>
+        <p className="card-date">
+              {dayOfWeekNames[dayjs(showtime.date).day()]}{" "}
+              {dayjs(showtime.date).tz(pacificTimeZone).format("MMM. D")}
+            </p>
+        <Card
+          showtime={showtime}
+          favorites={favorites}
+          setFavorites={setFavorites}
+          key={showtime['_id']}
+        />
+      </div>
     );
   });
   return (
     <div className='favorited-page'>
-      <h1 className='page-title fav-title'>Favorited</h1>
       <Link to='/'>
         <button className='favorites-nav-btn nav-btn'>back to home</button>
       </Link>
+      <h1 className='page-title fav-title'>Favorited</h1>
       {favorites.length === 0 ? (
         <div className='favorited-empty'>
             <img src={emptyBurst} className='favorited-burst'></img>
